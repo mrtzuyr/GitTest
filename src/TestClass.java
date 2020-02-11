59d5f3f66793cdf645d7583fb1a50cb3ee462645
mrtzuyr <56898395+mrtzuyr@users.noreply.github.com>
2020-02-11 16:07:58 -0600

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -10,19 +10,9 @@ public class TestClass {
 		
 		System.out.println("push test");
 
-<<<<<<< HEAD
-		System.out.println("heyyyyyy");
+
+		System.out.println("hmmjm test");
 		
-		
-		
-		
-		
-		System.out.println("olacak");
-=======
-		
-		
-		System.out.println("hello");
->>>>>>> 8bbe25204cc13ee861363bc33ad75e481db2b590
-		
+		System.out.println("pasddh test");
 	}
 }
