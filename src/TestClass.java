3b6fd746fed549f26e034493b67ae9dcd1b4a855
mrtzuyr <mrtzuyr@gmail.com>
2020-02-11 15:56:16 -0600

    sada

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,14 +2,13 @@
 public class TestClass {
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
+
 		
 		
 		System.out.println("this is master branch");
 		
-=======
-		System.out.println("This is from tester branch");
->>>>>>> tester
+
+
 		
 	}
 }
